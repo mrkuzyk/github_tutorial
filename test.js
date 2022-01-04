0c037f17b87e73cddd73a3ebbcdaeafc227a98de
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-05 00:11:41 +0200

    2-24

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
 
-  // Change code above this line
+
+  return (fruits.includes(fruit)); // Change this line
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(checkFruit("plum"));
