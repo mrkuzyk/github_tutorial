d5650e0a246c8806551dc438e956eeae6fd1f6dd
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-10 00:06:34 +0200

    2-32

--- test.js
+++ test.js
@@ -1,16 +1,13 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+function includes(array, value) {
+  // Change code below this line
+for (const arra of array)
+if (arra === value){
+return true
+}
 
+return false;
   // Change code above this line
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// функція шукає спільне значення в "арай" та "валуе"
+// без використання includes
