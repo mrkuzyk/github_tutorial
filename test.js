f526970f11594915b7439e49a456736a034f8e7b
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-09 22:17:41 +0200

    new

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
-function filterArray(numbers, value) {
-   // Change code below this line
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+
+  
+
+  for (const price of order) {
+    total += price;
+  }
 
   // Change code above this line
-},
+  return total;
+}
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// Інструкція перебирає "прайс" в "ордері" і потім всі прайси додає докупи
