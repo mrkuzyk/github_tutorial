ab797b7f867a1282d510b13b91eaf819850103aa
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-09 22:54:05 +0200

    2-27

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
 function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+  // Change code below this line
+  const filteredNumbers = [];
 
+  
+  for (const number of numbers){
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
   // Change code above this line
 }
 
 console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
 console.log(filterArray([12, 24, 8, 41, 76], 38));
 console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// функція вибирає методом перевірки числа більші за "валує" і добавляє ці числа до нової константи
