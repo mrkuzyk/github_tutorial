b34ecdf68a2d9012c05b3b046c3cf385da71b5bc
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-14 12:24:14 +0200

    3-8

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
 
+const product = {
+  // Change code below this line
+  name,
+  price,
+  image,
+  tags,
   // Change code above this line
-}
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// коли ім'я та значення мають оду і ту ж назву, то можна 
+// не писати це двічі.
