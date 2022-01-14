04a7cfc8b57d77b84c8f4b7fd94fa91035352483
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-14 17:47:13 +0200

    3-16

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+const values = Object.values(salaries);
 
+for (value of values){
+  totalSalary += value;
+}
   // Change code above this line
+  return totalSalary;
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(countTotalSalary({}));
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+// через "обжект.велю" ми отирмали всі значення зарплати
+// після чого перебрали всі дані і зробили їхню суму
