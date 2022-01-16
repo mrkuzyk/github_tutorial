3bf5805c9c4eeafb35b05b144d45b01c223cbb13
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-16 23:15:31 +0200

    3-38

--- test.js
+++ test.js
@@ -1,16 +1,12 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  addPotion(potionName) {
+    // Change code below this line
+    this.potions.push(potionName);
+  
+    // Change code above this line
+  },
+};
 
-  // Change code above this line
-}
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// добавляємо нове зілля до всіх
