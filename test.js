9f49c6b08297bcee46817d3862a8711fff6f2483
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-18 00:24:29 +0200

    4-7

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+function getCommonElements(firstArray, secondArray) {
+  const commonElements = [];
+  // Change code below this line
 
+//   for (let i = 0; i < firstArray.length; i += 1) {
+//     if (secondArray.includes(firstArray[i])) {
+//       commonElements.push(firstArray[i]);
+//     }
+//   }
+    
+    firstArray.forEach(function (array){
+    if(secondArray.includes(array)){
+      commonElements.push(array)
+    }
+  });
+
+  return commonElements;
   // Change code above this line
 }
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
