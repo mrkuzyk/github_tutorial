a394bef70a55d2e3780c2047f46c1a4752a35c88
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-17 23:50:03 +0200

    4-4

--- test.js
+++ test.js
@@ -1,16 +1,30 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+const pizzaPalace = {
+  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+  order(pizzaName, onSuccess, onError) {
+      if (!this.pizzas.includes(pizzaName)){
+        return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+      }
+      return onSuccess(pizzaName);
+  },
+};
+// Change code above this line
 
-  // Change code above this line
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+pizzaPalace.order('Smoked', makePizza, onOrderError);
+pizzaPalace.order('Four meats', makePizza, onOrderError);
+pizzaPalace.order('Big Mike', makePizza, onOrderError);
+pizzaPalace.order('Vienna', makePizza, onOrderError);
+
+
+// метод "ордер" приймає 3 параметри (останні з яких колбеки).
+// Перевіряємо чи є в "піззас" піца з назвою виклику і дивимося що вертати
