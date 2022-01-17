cec36ab4826d9f9f4cce3fb8bf0322368fd94713
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-18 00:29:16 +0200

    4-9

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
 
-  // Change code above this line
-}
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//   return quantity * pricePerItem;
+// };
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+// Change code above this line
