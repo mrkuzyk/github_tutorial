d459c15f1fe06bde8bfc7186a44db13e90c1bccf
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-18 23:36:27 +0200

    4-13

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+function changeEven(numbers, value) {
+  // Change code below this line
+    
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 === 0) {
+//       numbers[i] = numbers[i] + value;
+//     }
+//   }
+    
+    const newNumbers = [];
 
+  numbers.forEach(number => {
+    if (number % 2 === 0) {
+      newNumbers.push(number + value);
+    } else {
+      newNumbers.push(number)
+    }
+    
+  })
+  return newNumbers
   // Change code above this line
 }
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
