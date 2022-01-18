832f8fbbc799f42a8c41fdf47dd549caaaae0bd5
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-18 18:46:39 +0200

    4-11

--- test.js
+++ test.js
@@ -1,16 +1,23 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
+// Change code below this line
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   numbers.forEach(function (number) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+const filterArray = (numbers, value) => {
+  const filteredNumbers = [];
+
+  numbers.forEach((number) => {
+    if (number > value) {
+      filteredNumbers.push(number);
     }
-    return bigNumber;
+  });
 
   // Change code above this line
+  return filteredNumbers;
 }
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
