9450aa9bf94b5b085b334cfcd027bb72a6764fe9
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 23:51:49 +0200

    4-43

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const sortByDescendingFriendCount = users => {
+   const numbFriends = [...users].sort((a, b) => b.friends.length - a.friends.length);
+   return numbFriends;
+};
+// Change code above this line
 
-  // Change code above this line
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// кількість друзів по зменшенні
