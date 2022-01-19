c1c25a2d2d42cd243965fb14b5b2679d80dd6ac4
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 15:50:06 +0200

    4-23

--- test.js
+++ test.js
@@ -1,16 +1,9 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const getUsersWithAge = (users, minAge, maxAge) => {
+ const ageFilter = users.filter(user => user.age >= minAge && user.age <= maxAge);
+ return ageFilter;
 
-  // Change code above this line
-}
+};
+// Change code above this line
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// функція повертає користувачів вік яких між мінімальним і максимальним.
