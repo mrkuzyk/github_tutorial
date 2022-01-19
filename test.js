d7c70194f9476df7e7304e808d8121aa4dab9530
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 15:39:37 +0200

    4-22

--- test.js
+++ test.js
@@ -1,16 +1,9 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const getUsersWithEyeColor = (users, color) => {
+ const colorEye = users.filter(user => user.eyeColor === color);
+ return colorEye
 
-  // Change code above this line
-}
+};
+// Change code above this line
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// функція повертає користувачів, чий колірОчей === кольору
