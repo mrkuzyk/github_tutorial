cb239db63496cdf5b70bcaf1f52df139f1fde141
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 15:56:16 +0200

    4-24

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const getUsersWithFriend = (users, friendName) => {
+ const userFriend = users.filter(user => user.friends.includes(friendName))  
+ return userFriend
+};
+// Change code above this line
 
-  // Change code above this line
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// функція повертає тільки тих користувачів, у яких в друзях є "фріендНейм"
