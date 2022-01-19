703cc2334e32b6037a9d59c0e54f877c2dbdf764
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 16:07:25 +0200

    4-26

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const getActiveUsers = (users) => {
+   const activeUsers = users.filter(user => user.isActive === true);
+   return activeUsers;
+};
+// Change code above this line
 
-  // Change code above this line
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// функція повертає тільки активних користувачів
