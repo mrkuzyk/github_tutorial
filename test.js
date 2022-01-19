c54d127d94f84b97fc8e6a032f11ef675c68b136
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 18:01:50 +0200

    4-35

--- test.js
+++ test.js
@@ -1,16 +1,12 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+// Change code below this line
 
-  // Change code above this line
-}
+const totalAveragePlaytimePerGame = players.reduce((total, player) => (total += player.playtime / player.gamesPlayed), 0);
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// рахуємо суму середніх часів кожного гравця в грі
