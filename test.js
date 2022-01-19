3ea41053dc0c46598a802face260f7091fea6e71
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 18:09:07 +0200

    4-36

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const calculateTotalBalance = users => {
+    const sumUsers = users.reduce((total, user) => total += user.balance, 0);
+    return sumUsers;
+};
+// Change code above this line
 
-  // Change code above this line
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// рахуємо суму всіх коштів користувачів
