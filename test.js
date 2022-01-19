d64cb95e2aa1ced393ff5fdeab8eea5911fac2fd
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 16:03:07 +0200

    4-25

--- test.js
+++ test.js
@@ -1,16 +1,10 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const getFriends = (users) => {
+    const allFriends = users.flatMap(user => user.friends);
+    const mutualFriends = allFriends.filter((user, index, array) => array.indexOf(user) === index);
+    return mutualFriends;
+};
+// Change code above this line
 
-  // Change code above this line
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// функція спочатку створює масив всіх друзів, а потім залишає
+// тільки різних ім'я (не повторюються)
