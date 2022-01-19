acaf107a85d2be3d98b43e03a30869c3b2df1a44
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-20 00:16:22 +0200

    4-46

--- test.js
+++ test.js
@@ -1,16 +1,10 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const getNamesSortedByFriendCount = users => {
+   const sortName = [...users]
+    .sort((a, b) => a.friends.length - (b.friends.length))
+    .map(user => user.name);
+    return sortName;
+};
+// Change code above this line
 
-  // Change code above this line
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// спочатку сортуємо по кількості друзів, а потім робимо з того масив ім'я
