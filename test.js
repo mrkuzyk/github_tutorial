f8f26c76193810569352c4882b9c7d136ebe2bcf
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 14:32:11 +0200

    4-18

--- test.js
+++ test.js
@@ -1,16 +1,10 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+// Change code below this line
+const getUserEmails = users => {
+    const email = users.map(user => user.email);
+    return email
 
+  };
   // Change code above this line
-}
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+//   функція повертає масив поштових адрес користувачів
