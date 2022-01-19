7cb76f06cf270c96133d441db2ca0c42218102ef
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-19 14:45:30 +0200

    4-19

--- test.js
+++ test.js
@@ -1,16 +1,7 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// Change code below this line
 
-  // Change code above this line
-}
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+const oddNumbers = numbers.filter(number => number % 2 === 1);
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// перша перемінна вертає парні числа, а друга непарні
