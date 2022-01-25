d82ec3146571adc613c2b015c7fe921aedd79439
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-26 00:43:49 +0200

    5-4

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+const parent = {
+  name: "Stacey",
+  surname: "Moore",
+  age: 54,
+  heritage: "Irish",
+};
+// Change code below this line
 
-  // Change code above this line
-}
+const child = Object.create(parent);
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+// Change code above this line
+child.name = "Jason";
+child.age = 27;
+
+
+// створення прототипу "парент"
