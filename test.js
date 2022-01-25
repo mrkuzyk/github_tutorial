104ec063f901d50e745a8ba8b99b5ad93a049679
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-26 00:46:26 +0200

    5-5

--- test.js
+++ test.js
@@ -1,16 +1,21 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+const ancestor = {
+  name: "Paul",
+  age: 83,
+  surname: "Dawson",
+  heritage: "Irish",
+};
+// Change code below this line
 
-  // Change code above this line
-}
+const parent = Object.create(ancestor);
+parent.name = "Stacey";
+parent.surname = "Moore";
+parent.age = 54;
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+const child = Object.create(parent);
+child.name = "Jason";
+child.age = 27;
+
+// Change code above this line
+
+
+// "Анцестор є прототипом для "парент", а ті є прототипом для "чайлд"
