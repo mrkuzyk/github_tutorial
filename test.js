9ae370de0fc13be184242961fe7ee4cdf66f9119
Marian Kuzyk <mariankuzyk@gmail.com>
2022-01-27 23:17:02 +0200

    5-9

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
-function filterArray(numbers, value) {
-   // Change code below this line
-let bigNumber = [];
-    for (let i = 0; i < numbers.length; i += 1){
-        if (numbers[i] > value) {
-            bigNumber.push(numbers[i]);
-        }
-    }
-    return bigNumber;
+class Car {
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+  // Change code below this line
+  getPrice(){
+  return this.price;
+  }
+
+  changePrice(newPrice){
+  this.price = newPrice;
+  }
 
   // Change code above this line
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+  console.log({ brand: 'Audi', model: 'Q3', price: 36000 })
+
